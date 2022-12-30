9322acb17bed2e017612d2d2ee4f3243c4de5d9d
David <dadastudios382@gmail.com>
2022-12-30 11:07:33 -0500

    solving responsive design issues

--- src/components/Offers/CardList.tsx
+++ src/components/Offers/CardList.tsx
@@ -2,7 +2,7 @@ import Card from "./Card";
 
 const CardList = () => {
     return (
-        <ul className="max-w-screen-xl h-full mx-auto py-48 px-10 grid grid-cols-3 items-center justify-center gap-10 ">
+        <ul className="max-w-screen-xl h-full mx-auto py-48 px-10 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 items-center justify-center gap-10 ">
             <Card
                 icon={<svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>}
 
